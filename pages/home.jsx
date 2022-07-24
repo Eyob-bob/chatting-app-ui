9c6d6e7f3f1fe1dd89f6d40839556bffe2f64187
Eyob Abebe <eabebe91@gmail.com>
2022-07-24 15:45:19 +0300

    adding some styles

--- pages/home.jsx
+++ pages/home.jsx
@@ -80,7 +80,7 @@ export default function Home() {
       </Head>
       <Navbar username={dataUser.user.username} email={dataUser.user.email} />
       <div className="min-h-screen w-screen flex justify-center">
-        <div className="text-center max-w-[760px] w-[100%] flex flex-col justify-center items-center mt-20 py-10 mx-20 my-auto border md:mx-10 sm:mx-2">
+        <div className="text-center max-w-[950px] w-[100%] flex flex-col justify-center items-center mt-20 py-10 mx-20 my-auto border md:mx-10 sm:mx-2">
           <Flex className=" w-[100%] md:flex-col p-10 py-0">
             <div className=" border md:w-full md:h-fit w-[35%] h-[60vh] overflow-y-scroll no-scrollbar px-4 py-2 flex flex-col gap-4 shadow-inner rounded-md ">
               <h1 className="font-bold text-lg">Chats</h1>
